2ed33805fe8ad6f617fecc9cca468b6023a5392a
pranav <pranavsdharanioathy@gmail.com>
2025-02-09 13:10:36 -0600

    first commit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tuning/AdvancedMotionTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tuning/AdvancedMotionTest.java
@@ -3,7 +3,7 @@ package org.firstinspires.ftc.teamcode;
 import com.acmerobotics.roadrunner.Pose2d;
 import com.acmerobotics.roadrunner.ProfileAccelConstraint;
 import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
-import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.TranslationalVelConstraint;
 import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
@@ -19,15 +19,16 @@ public class AdvancedMotionTest extends LinearOpMode {
         TrajectoryActionBuilder advancedMotionTest = drive.actionBuilder(startPose)
                 .lineToXLinearHeading(48, Math.toRadians(90))
                 .lineToXLinearHeading(0, Math.toRadians(0))
-                .splineToLinearHeading(new Pose2d(50, 0, Math.toRadians(180)), Math.toRadians(0),
-                null,
-                new ProfileAccelConstraint(-15.0, 15.0))
-                .waitSeconds(1.25)
-                .turnTo(90)
-                .strafeToLinearHeading(new Vector2d(0,50), Math.toRadians(0));
+                .splineToLinearHeading(new Pose2d(60, 0, Math.toRadians(180)), Math.toRadians(0),
+                new TranslationalVelConstraint(50),
+                new ProfileAccelConstraint(-20.0, 20.0))
+                .splineToLinearHeading(new Pose2d(0, 0, Math.toRadians(0)), Math.toRadians(0));
 
         waitForStart();
+        if (isStopRequested()) return;
 
-        Actions.runBlocking(advancedMotionTest.build());
+        while (opModeIsActive()) {
+            Actions.runBlocking(advancedMotionTest.build());
+        }
     }
 }
