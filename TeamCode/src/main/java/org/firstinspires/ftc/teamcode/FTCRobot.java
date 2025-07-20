8946b90d4130809f528a7fae595735b6f8cdaf46
ze-NICKEL <nsarathy3@gmail.com>
2025-07-20 17:00:56 -0500

    Created FTCRobot Class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FTCRobot.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+public class FTCRobot {
+    private DcMotor leftFront;
+    private DcMotor rightFront;
+    private DcMotor leftBack;
+    private DcMotor rightBack;
+}
